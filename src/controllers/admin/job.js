1db82efb36d5499779612877b6107f5331315372
Ijsharma123 <inderjeetsharma723@gmail.com>
2023-03-04 13:16:02 +0530

    changes

--- src/controllers/admin/job.js
+++ src/controllers/admin/job.js
@@ -45,76 +45,14 @@ exports.jobAdd = async function jobAdd(req, res) {
 
 
 /** Job List */
-exports.jobList = async function jobList(req, res) {
-    try {
-        let matchobj = {}
-        if (req.query.assign_to) {
-            matchobj['assign_to'] = mongoose.Types.ObjectId(req.query.assign_to)
-        }
-        const jobList = await Job.aggregate([
-            {
-                $match: { ...matchobj }
-            },
-            {
-                $lookup: {
-                    from: 'users',
-                    localField: 'assign_to',
-                    foreignField: '_id',
-                    as: 'user'
-                }
-            },
-            { $unwind: "$user" },
-            {
-                $project: {
-                    _id: 1,
-                    client_name: 1,
-                    project_name: 1,
-                    address: 1,
-                    notes: 1,
-                    site_work: 1,
-                    assign_to: 1,
-                    assign_to_name: "$user.name",
-                    progress: 1,
-                    due_date: 1,
-                }
-            }
-
-        ])        // const jobList = await Job.find().sort({ addedAt: -1 })
-        // const jobArr = [];
-
-        // for (const [_, value] of Object.entries(jobList)) {
-        //     const userArr = value.assign_to;
-        //     var user = '';
-        //     var user_names = '';
-        //     for (let key in userArr) {
-        //         var user = await User.findOne({ _id: userArr[key] });
-        //         if (user != null) {
-        //             user_names += user.name + ', ';
-        //         }
-        //     }
-
-
-        //     const job = {
-        //         _id: value._id,
-        //         client_name: value.client_name,
-        //         project_name: value.project_name,
-        //         address: value.address,
-        //         notes: value.notes,
-        //         site_work: value.site_work,
-        //         assign_to: value.assign_to,
-        //         assign_to_name: user_names,
-        //         progress: value.progress,
-        //         due_date: value.due_date,
-        //     }
-        //     jobArr.push(job);
-        // }
-
-        return res.status(200).globalResponse({ success: true, message: 'count ' + jobList.length, data: jobList })
-    } catch (err) {
-        return res.status(401).globalResponse({ success: false, message: err.message })
+exports.jobList = async function jobList(req,res){
+    try{
+        const jobList = await Job.find()
+        return res.status(200).globalResponse({success:true, count:jobList.length, data:jobList})
+    }catch(err){
+        return res.status(401).globalResponse({success:false, msg:err.message})
     }
 }
-
 
 
 /** Job Update */
